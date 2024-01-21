5bb904da709a1f2dd345bebbcec8ac96c5c760b0
Nichole Turner <nicholeturner@Nicholes-MacBook-Air.local>
2024-01-21 13:39:35 -0500

    3-server-side-views-and-rendering-jsx

--- server.js
+++ server.js
@@ -7,21 +7,26 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads!')
 })
 
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
+
+
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
-// ROUTES
-app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads')
-  })
   
-  // Breads
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
+  
   
