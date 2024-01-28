2817afedfcd2f69ce04c6174f303db419e36b224
Nichole Turner <nicholeturner@Nicholes-MacBook-Air.local>
2024-01-28 13:48:19 -0500

    add nonogoose

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 
 // DEPENDENCIES
 const express = require('express')
+const mongoose = require('mongoose')
+
 
 // CONFIGURATION
 require('dotenv').config()
@@ -34,6 +36,11 @@ app.listen(PORT, () => {
 app.get('*', (req, res) => {
   res.send('404')
 })
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
+
 
   
   
